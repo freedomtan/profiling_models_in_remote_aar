e0cea5d9faa6c52b651c5ce0abfd0999c49b2c2c
Koan-Sin Tan <koansin.tan@gmail.com>
2024-08-25 19:43:27 +0800

    Initial Commit

---
+++ profileMlodelcInRemoteAAR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  profileMlodelcInRemoteAAR
+//
+//  Created by Koan-Sin Tan on 8/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ profileMlodelcInRemoteAAR/profileMlodelcInRemoteAARApp.swift
@@ -0,0 +1,17 @@
+//
+//  profileMlodelcInRemoteAARApp.swift
+//  profileMlodelcInRemoteAAR
+//
+//  Created by Koan-Sin Tan on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct profileMlodelcInRemoteAARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
